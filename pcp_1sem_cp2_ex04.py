227788843ea86dbabaca709483a57a2e9cdaa73a
Zanizada <rafaelgizzi@gmail.com>
2026-04-17 00:45:20 -0300

    Otimização do exercício 4 com o uso de funções para a maior parte dos cálculos, levando em consideração também os requisitos do checkpoint (sem funções embutidas e com funções criadas pelo dev).

--- pcp_1sem_cp2_ex04.py
+++ pcp_1sem_cp2_ex04.py
@@ -1,33 +1,52 @@
+# Funções
+def calcular_horas_extras(salario_base, horas):
+    return salario_base * 0.015 * horas
+
+def calcular_descontos_faltas(salario_base, faltas):
+    return salario_base * 0.02 * faltas
+
+def calcular_bonus(cargo, recebeu_bonus):
+    match recebeu_bonus:
+        case "s":
+            match cargo:
+                case 1:
+                    return 1000
+                case 2:
+                    return 500
+                case 3:
+                    return 300
+                case 4:
+                    return 100
+        case "n":
+            return 0
+
+def calcular_salario_bruto(salario, bonus, valor_hora):
+    return salario + bonus + valor_hora
+
+def calcular_acrescimos(bonus, valor_hora):
+    return bonus + valor_hora
+
+def calcular_salario_final(salario_bruto, desconto_faltas):
+    return salario_bruto - desconto_faltas
+
 print("Cálculo do salário final")
 
 nome = input("Digite seu nome: ")
-cargo = input("Digite seu cargo: ").lower().strip()
+cargo = int(input("--- CARGOS ---\n1-Gerente\n2-Analista\n3-Assistente\n4-Estagiário\nDigite seu cargo: "))
 salario = float(input("Digite seu salario base: "))
 horas_extras = int(input("Digite a quantidade de horas extras trabalhadas: "))
 faltas = int(input("Digite a quantidade de faltas no mês: "))
-bonus = input("Digite se recebeu bonus por desempenho: ").lower().strip()
-valor_bonus = 0
+bonus = input("Digite se recebeu bonus por desempenho (s ou n): ")
 
-valor_hora_extra = salario * 0.015 * horas_extras
-desconto_falta = salario * 0.02 * faltas
+valor_hora_extra = calcular_horas_extras(salario, horas_extras)
+desconto_falta = calcular_descontos_faltas(salario, faltas)
+valor_bonus = calcular_bonus(cargo, bonus)
 
-if bonus == "s" or bonus == "sim":
-    if cargo == "gerente":
-        valor_bonus = 1000
-    elif cargo == "analista":
-        valor_bonus = 500
-    elif cargo == "assistente":
-        valor_bonus = 300
-    elif cargo == "estagiário" or cargo == "estagiario":
-        valor_bonus = 100
-    else:
-        print("cargo invalido")
+salario_bruto = calcular_salario_bruto(salario, valor_bonus, valor_hora_extra)
+total_acrescimos = calcular_acrescimos(valor_bonus, valor_hora_extra)
+salario_final = calcular_salario_final(salario_bruto, desconto_falta)
 
-salario_bruto = salario + valor_bonus + valor_hora_extra
-total_acrescimos = valor_bonus + valor_hora_extra
-salario_final = salario + valor_hora_extra + valor_bonus - desconto_falta
-
-print(f"Seu salário bruto é: {salario_bruto}")
-print(f"Seu total em acréscimos foi de: {total_acrescimos}  ")
-print(f"Seu desconto foi de {desconto_falta}  ")
-print(f"Seu salário final foi de: {salario_final}")
+print(f"Seu salário bruto é: R$ {salario_bruto:.2f}")
+print(f"Seu total em acréscimos foi de: R$ {total_acrescimos:.2f}")
+print(f"Seu desconto foi de: R$ {desconto_falta:.2f}")
+print(f"Seu salário final foi de: R$ {salario_final:.2f}")
