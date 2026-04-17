dcbc1b361946d2274a50755536f1863cfddf558b
Zanizada <rafaelgizzi@gmail.com>
2026-04-16 23:54:34 -0300

    Otimização do exercício 3 transformando alguns cálculos em funções.

--- pcp_1sem_cp2_ex03.py
+++ pcp_1sem_cp2_ex03.py
@@ -1,3 +1,21 @@
+# Funções
+def encontrar_menor(nota1, nota2, nota3):
+    menor = nota1
+    if nota2 < menor:
+        menor = nota2
+    if nota3 < menor:
+        menor = nota3
+    return menor
+
+def calculo_media_semestre(nota1, nota2, nota3, nota_sp1, nota_sp2, nota_gs):
+    menor = encontrar_menor(nota1, nota2, nota3)
+    # A fórmula utiliza: (soma dos 2 maiores CPs + soma das 2 Sprints) / 4
+    media_atividades = (nota1 + nota2 + nota3 - menor + nota_sp1 + nota_sp2) / 4
+    return (media_atividades * 0.4) + (nota_gs * 0.6)
+
+def calculo_media_peso(media):
+    return media * 0.4
+
 # 1 Solicitar notas
 cp1 = float(input("Nota do Checkpoint 1: "))
 cp2 = float(input("Nota do Checkpoint 2: "))
@@ -7,21 +25,13 @@ sp2 = float(input("Nota da Sprint 2: "))
 gs = float(input("Nota da Global Solution: "))
 
 # 2 Identificar menor nota entre os 3 checkpoints
-menor = cp1
-
-if cp2 < menor:
-    menor = cp2
-
-if cp3 < menor:
-    menor = cp3
+menor = encontrar_menor(cp1, cp2, cp3)
 
 # 3 Cálculo da média do semestre (sem peso)
-# A fórmula utiliza: (soma dos 2 maiores CPs + soma das 2 Sprints) / 4
-media_atividades = (cp1 + cp2 + cp3 - menor + sp1 + sp2) / 4
-media_semestre = (media_atividades * 0.4) + (gs * 0.6)
+media_semestre = calculo_media_semestre(cp1, cp2, cp3, sp1, sp2, gs)
 
 # 4 Cálculo da média com peso 40%
-media_peso = media_semestre * 0.4
+media_peso = calculo_media_peso(media_semestre)
 
 # 5 Resultado decimal
 print(f"Menor nota de Checkpoint desconsiderada: {menor:.1f}")
